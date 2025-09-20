da2258c71e9d9faf60c638c20e4a2d6bf4840e27
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-09-20 11:10:41 -0500

    api_node

---
+++ js/02_proj_start/backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const verifyToken = require('../middlewares/authMiddleware');
+
+router.post('/login', userController.login);
+router.post('/create', userController.register);
+
+router.get('/', verifyToken, userController.getAllUsers);
+router.get('/:id', verifyToken, userController.getUserById);
+router.put('/:id', verifyToken, userController.getUserUpdate);
+router.delete('/delete/:id', verifyToken, userController.getUserDelete);
+
+module.exports = router;
